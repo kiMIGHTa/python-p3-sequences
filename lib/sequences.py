2400423f23a83c0fddefb27d6e9efae7698db1ef
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-22 12:35:50 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_sequence=[]
+    if length==1:
+        fib_sequence=[0]
+    if length>=2:
+        fib_sequence=[0,1]
+        for i in range(2,length):
+            next_fib=fib_sequence[i-1] + fib_sequence[i-2]
+            fib_sequence.append(next_fib)
+    
+    print(fib_sequence)
+    
     pass
